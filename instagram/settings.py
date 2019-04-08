cf64de14e3a692842cba6d75c76882cfc4bb8f73
Minsu Kang <kms920612@gmail.com>
2019-04-08 04:44:30 +0000

    posts 모델 생성

--- instagram/settings.py
+++ instagram/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'posts',
 ]
 
 ROOT_URLCONF = 'instagram.urls'
